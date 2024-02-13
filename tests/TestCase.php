f20fdd564469ad67f65e1684213fbf874ef75219
Everton Paulouski <paulouskipsk@gmail.com>
2024-02-12 23:31:24 -0300

    ADD teste authController

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function getHeader(){
+        $params = ['login' => 'admin', 'password' =>'123'];
+        $response = $this->post('/api/auth/authenticate',  $params, [ 'Accept', 'application/json']);
+        $header = [ 'Accept', 'application/json', 'Authorization' => "Bearer ". $response['payload']['token']];
+        return $header;
+    }
 }
